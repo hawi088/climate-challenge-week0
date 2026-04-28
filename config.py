23ec64a3aa00009cdadd8afb0c242e1da3358fdf
hawi088 <hawichalaf8@gmail.com>
2026-04-28 10:09:36 +0300

    feat: Added config.py for better centralization

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+COUNTRIES = ['ethiopia', 'kenya', 'sudan', 'tanzania', 'nigeria']
+WEATHER_COLS = ['T2M', 'T2M_MAX', 'T2M_MIN', 'PRECTOTCORR', 'RH2M', 'WS2M']
+HEAT_THRESHOLD = 35  # °C
+DRY_THRESHOLD = 1    # mm
+OUTLIER_THRESHOLD = 3
